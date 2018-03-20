b8cec54b312ee87ec28d4dd7b212082b65ff1170
Ben Croker <ben@putyourlightson.net>
2018-03-20 02:15:28 +0100

    Added table column default values

---
+++ src/migrations/m180320_120000_add_default_values.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace putyourlightson\campaign\migrations;
+
+use craft\db\Migration;
+use craft\helpers\MigrationHelper;
+
+/**
+ * m180320_120000_add_default_values migration.
+ */
+class m180320_120000_add_default_values extends Migration
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('{{%campaign_campaigns}}', 'recipients', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_campaigns}}', 'opened', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_campaigns}}', 'clicked', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_campaigns}}', 'opens', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_campaigns}}', 'clicks', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_campaigns}}', 'complained', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_campaigns}}', 'unsubscribed', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_campaigns}}', 'bounced', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_links}}', 'clicked', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_links}}', 'clicks', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_contacts_campaigns}}', 'opens', $this->integer()->defaultValue(0)->notNull());
+        $this->alterColumn('{{%campaign_contacts_campaigns}}', 'clicks', $this->integer()->defaultValue(0)->notNull());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180320_120000_add_default_values cannot be reverted.\n";
+
+        return false;
+    }
+}
