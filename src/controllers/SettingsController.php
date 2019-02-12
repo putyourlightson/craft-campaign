4bf9d1f5407194cdff35278e9197c38b240ca740
Ben Croker <ben@putyourlightson.net>
2019-02-12 17:32:24 +0530

    Env vars no longer parsed to plaintext

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -68,7 +68,6 @@ class SettingsController extends Controller
 
         return $this->renderTemplate('campaign/settings/general', [
             'settings' => $settings,
-            'parsedApiKey' => Craft::parseEnv($settings->apiKey),
             'config' => Craft::$app->getConfig()->getConfigFromFile('campaign'),
             'phpBinPath' => PHP_BINARY ?: '/usr/bin/php',
             'isWebAliasUsed' => Campaign::$plugin->settings->isWebAliasUsed(),
