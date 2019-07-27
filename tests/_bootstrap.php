cc935249838d96751a10917745f4ee1dd013c3dc
Ben Croker <ben@putyourlightson.net>
2019-07-27 23:36:32 +0200

    Fixed testing bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,14 +4,14 @@ use craft\test\TestSetup;
 
 ini_set('date.timezone', 'UTC');
 
-// Use the current installation of Craft
 define('CRAFT_STORAGE_PATH', __DIR__ . '/_craft/storage');
 define('CRAFT_TEMPLATES_PATH', __DIR__ . '/_craft/templates');
 define('CRAFT_CONFIG_PATH', __DIR__ . '/_craft/config');
 define('CRAFT_MIGRATIONS_PATH', __DIR__ . '/_craft/migrations');
 define('CRAFT_TRANSLATIONS_PATH', __DIR__ . '/_craft/translations');
-define('CRAFT_VENDOR_PATH', dirname(__DIR__).'/vendor');
+//define('CRAFT_VENDOR_PATH', dirname(__DIR__, 3));
 
-$devMode = true;
+// Use absolute path if the plugin directory is a symlink
+define('CRAFT_VENDOR_PATH', '/Users/ben/Sites/craft32/vendor');
 
 TestSetup::configureCraft();
