6b4c055b96d45962976b71f6de5d3654d9d67d09
bencroker <ben@putyourlightson.com>
2022-06-03 10:56:20 +0200

    Fix migration

--- src/migrations/m220427_120000_rename_configsetting.php
+++ src/migrations/m220427_120000_rename_configsetting.php
@@ -18,9 +18,9 @@ class m220427_120000_rename_configsetting extends Migration
         $schemaVersion = $projectConfig->get('plugins.campaign.schemaVersion', true);
 
         if (version_compare($schemaVersion, '2.0.0', '<')) {
-            $maxSendFailuresAllowed = $projectConfig->get('campaign.settings.maxSendFailsAllowed', true) ?? 1;
+            $maxSendFailuresAllowed = $projectConfig->get('plugins.campaign.settings.maxSendFailsAllowed', true) ?? 1;
 
-            $projectConfig->set('campaign.settings.maxSendFailuresAllowed', $maxSendFailuresAllowed);
+            $projectConfig->set('plugins.campaign.settings.maxSendFailuresAllowed', $maxSendFailuresAllowed);
         }
 
         return true;
