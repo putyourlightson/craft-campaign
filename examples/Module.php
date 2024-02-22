82477e70e059faf55376668c6bf596cd6a27a916
bencroker <ben@putyourlightson.com>
2024-02-22 11:20:05 -0600

    Add ContactCondition to example Module

--- examples/Module.php
+++ examples/Module.php
@@ -5,9 +5,11 @@ namespace modules;
 use Craft;
 use craft\base\conditions\BaseCondition;
 use craft\events\RegisterConditionRuleTypesEvent;
+use modules\conditions\segments\ContactIsUserConditionRule;
 use modules\conditions\sendouts\LastEntryHasImageConditionRule;
 use modules\conditions\sendouts\MondayMorningSendoutConditionRule;
 use modules\conditions\sendouts\RecentEntriesPublishedConditionRule;
+use putyourlightson\campaign\elements\conditions\contacts\ContactCondition;
 use putyourlightson\campaign\elements\conditions\sendouts\SendoutScheduleCondition;
 use yii\base\Event;
 
@@ -18,6 +20,14 @@ class Module extends \yii\base\Module
         Craft::setAlias('@modules', __DIR__);
 
         parent::init();
+
+        Event::on(
+            ContactCondition::class,
+            BaseCondition::EVENT_REGISTER_CONDITION_RULE_TYPES,
+            function(RegisterConditionRuleTypesEvent $event) {
+                $event->conditionRuleTypes[] = ContactIsUserConditionRule::class;
+            }
+        );
 
         Event::on(
             SendoutScheduleCondition::class,
