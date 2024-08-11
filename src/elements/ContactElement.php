fbf073bd25efdbc0c2b8f5ae416f89bceee98ab6
bencroker <ben@putyourlightson.com>
2024-08-11 11:12:43 +0200

    Tweak fill

--- src/elements/ContactElement.php
+++ src/elements/ContactElement.php
@@ -788,7 +788,7 @@ class ContactElement extends Element
 
         return <<<XML
             <svg version="1.1" baseProfile="full" width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="50" cy="50" r="50" fill="var(--gray-200)"/>
+                <circle cx="50" cy="50" r="50" fill="var(--gray-500)" fill-opacity=".15"/>
                 <text x="50" y="66" font-size="46" font-weight="500" font-family="sans-serif" text-anchor="middle" fill="var(--gray-700)">$initials</text>
             </svg>
         XML;
