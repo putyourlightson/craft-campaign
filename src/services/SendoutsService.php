377a7eee65b30981a62b0ffc631ca81d0462f463
bencroker <ben@putyourlightson.com>
2026-06-01 14:50:45 +0200

    Mark sendout failed and log message

--- src/services/SendoutsService.php
+++ src/services/SendoutsService.php
@@ -235,6 +235,13 @@ class SendoutsService extends Component
         $campaign = $sendout->getCampaign();
 
         if ($campaign === null || !$campaign->hasSendableStatus()) {
+            $sendout->sendStatus = SendoutElement::STATUS_FAILED;
+            $this->updateSendoutRecord($sendout, ['sendStatus']);
+
+            Campaign::$plugin->log('Sending of the sendout “{title}” failed due to the campaign no longer being available or sendable.', [
+                'title' => $sendout->title,
+            ]);
+
             return;
         }
 
@@ -278,7 +285,6 @@ class SendoutsService extends Component
         } /** @noinspection PhpRedundantCatchClauseInspection */
         catch (Error) {
             $sendout->sendStatus = SendoutElement::STATUS_FAILED;
-
             $this->updateSendoutRecord($sendout, ['sendStatus']);
 
             Campaign::$plugin->log('Sending of the sendout “{title}” failed due to a Twig error when rendering the template.', [
