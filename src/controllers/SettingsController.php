52f9a1ad5ce72356cdb5ef616099545d90d5533f
bencroker <ben@putyourlightson.com>
2026-06-05 12:33:57 +0200

    Add `Disable Link Tracking` sendout setting
    https://github.com/putyourlightson/craft-campaign/issues/559

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -280,6 +280,7 @@ class SettingsController extends BaseSettingsController
 
         // Set the simple stuff
         $settings->defaultNotificationContactIds = $this->request->getBodyParam('defaultNotificationContactIds', $settings->defaultNotificationContactIds) ?: null;
+        $settings->disableLinkTracking = $this->request->getBodyParam('disableLinkTracking', $settings->disableLinkTracking) ?: false;
         $settings->showSendoutTitleField = $this->request->getBodyParam('showSendoutTitleField', $settings->showSendoutTitleField) ?: false;
         $settings->sendoutJobBatchSize = $this->request->getBodyParam('sendoutJobBatchSize', $settings->sendoutJobBatchSize) ?: null;
         $settings->sendoutJobBatchDelay = $this->request->getBodyParam('sendoutJobBatchDelay', $settings->sendoutJobBatchDelay) ?: 0;
